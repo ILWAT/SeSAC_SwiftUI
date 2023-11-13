d5a279d7544e41eaf483aa90d82346c80773568c
J.H. Moon <moonjh587@naver.com>
2023-11-13 09:42:36 +0900

    Initial Commit

---
+++ SeSAC_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSAC_SwiftUI
+//
+//  Created by 문정호 on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeSAC_SwiftUI/SeSAC_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSAC_SwiftUIApp.swift
+//  SeSAC_SwiftUI
+//
+//  Created by 문정호 on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SeSAC_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
